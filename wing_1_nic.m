9e32b92aa7614fabc358d0874f892acf425a70ef
Nicolas Förster <nicolasfoerster@Nicolass-MBP-5.connect>
2019-02-04 12:45:29 +0000

    My own wing code kept separate from Geralds for clarity

--- wing_1_nic.m
+++ wing_1_nic.m
@@ -242,10 +242,11 @@ hold off
 
 figure
 hold on
-plot(x,t_front,'b')
-plot(x,t_rear,'r')
-title('spar web thickness')
-legend('front','rear')
+stairs(x,t_front,'b')
+stairs(x,t_rear,'r')
+legend('Front spar','Rear spar')
+xlabel('Span-wise distance from root (m)')
+ylabel('Spar web (m)')
 hold off
 
 % incorporating manufacturing constraints into design
@@ -323,16 +324,18 @@ end
 
 figure
 hold on
-plot(x,t_front_min_1,'-.','LineWidth',1.5)
-plot(x,t_rear_min_1,'-.','LineWidth',1.5)
-plot(x,t_front_min_2,'--','LineWidth',1.5)
-plot(x,t_rear_min_2,'--','LineWidth',1.5)
+plot(x,t_front_min_1,':','LineWidth',2.5)
+plot(x,t_rear_min_1,':','LineWidth',2.5)
+plot(x,t_front_min_2,'--','LineWidth',2)
+plot(x,t_rear_min_2,'--','LineWidth',2)
+xlabel('Span-wise distance from root (m)')
+ylabel('Spar web (m)')
 legend('Manufacturing constrained front spar','Manufacturing constrained rear spar','Optimised front spar','Optimised rear spar')
 hold off
-%%
+
 % calculating shear stress in spar webs
-shear_front_spar = totalShearForce ./ (2 .* h_wingBox .* t_front);
-shear_rear_spar = totalShearForce ./ (2 .* h_wingBox .* t_rear);
+shear_front_spar = totalShearForce ./ (2 .* h_wingBox .* t_front_min_2);
+shear_rear_spar = totalShearForce ./ (2 .* h_wingBox .* t_rear_min_2);
 
 % max shear stress must be below yield stress
 shear_front_max = max(shear_front_spar);
@@ -387,73 +390,263 @@ leg3 = plot([0.15,0.65,0.65,0.15,0.15],[-0.035,-0.035,0.065,0.065,-0.035],'bo-.'
 legend([leg1,leg2,leg3],'NACA profile','Real wing box section','Idealised rectangular wing box section')
 hold off
 
-
-%% integrally stiffened panels ideal
+%% Digitalising Catchpole Diagram
 close all
+
+% R_t = 1.0
+fid = fopen('R_t_1_0.txt');
+vals_1 = fscanf(fid, '%f %f ',[2 Inf]);
+catchpole_1 = transpose(vals_1);
+fclose(fid);
+
+% R_t = 1.5
+fid = fopen('R_t_1_5.txt');
+vals_2 = fscanf(fid, '%f %f ',[2 Inf]);
+catchpole_2 = transpose(vals_2);
+fclose(fid);
+
+% R_t = 2.0
+fid = fopen('R_t_2_0.txt');
+vals_3 = fscanf(fid, '%f %f ',[2 Inf]);
+catchpole_3 = transpose(vals_3);
+fclose(fid);
+
+% R_t = 2.5
+fid = fopen('R_t_2_5.txt');
+vals_4 = fscanf(fid, '%f %f ',[2 Inf]);
+catchpole_4 = transpose(vals_4);
+fclose(fid);
+
+% R_t = 5.0
+fid = fopen('R_t_5_0.txt');
+vals_5 = fscanf(fid, '%f %f ',[2 Inf]);
+catchpole_5 = transpose(vals_5);
+fclose(fid);
+
+figure
+hold on
+plot(catchpole_1(:,1),catchpole_1(:,2),'r-')
+plot(catchpole_2(:,1),catchpole_2(:,2),'b-')
+plot(catchpole_3(:,1),catchpole_3(:,2),'r-.')
+plot(catchpole_4(:,1),catchpole_4(:,2),'b-.')
+plot(catchpole_5(:,1),catchpole_5(:,2),'r--')
+xlabel('$\frac{h}{b}$','Interpreter','latex','FontSize',20)
+ylabel('$\frac{\sigma_{cr}}{\sigma_0}$','Interpreter','latex','FontSize',20)
+hold off
+
+% possible parameter values
+R_t = [1.0; 1.5; 2.0; 2.5; 5.0];
+R_b = [0.2; 0.4; 0.6; 0.8; 1.0; 1.2];
+% sigma ratio at R_b values in previous line for different R_t curves
+sigma_ratio(1,[1:6]) = [catchpole_1(9,2), catchpole_1(19,2), catchpole_1(31,2), catchpole_1(42,2), catchpole_1(53,2), catchpole_1(63,2)]; % R_t = 1.0
+sigma_ratio(2,[1:6]) = [catchpole_2(7,2), catchpole_2(10,2), catchpole_2(20,2), catchpole_2(33,2), catchpole_2(44,2), catchpole_2(52,2)]; % R_t = 1.5
+sigma_ratio(3,[1:6]) = [catchpole_3(16,2), catchpole_3(26,2), catchpole_3(35,2), catchpole_3(51,2), catchpole_3(63,2), catchpole_3(73,2)]; % R_t = 2.0
+sigma_ratio(4,[1:6]) = [catchpole_4(16,2), catchpole_4(27,2), catchpole_4(37,2), catchpole_4(49,2), catchpole_4(65,2), catchpole_4(76,2)]; % R_t = 2.5
+sigma_ratio(5,[1:6]) = [catchpole_5(16,2), catchpole_5(27,2), catchpole_5(37,2), catchpole_5(48,2), catchpole_5(63,2), catchpole_5(73,2)]; % R_t = 5.0
+
+
+%% integrally stiffened panels ideal - LOOK HERE
+close all
+clc
 % cover load per unit length N
 h_wingBox = 0.10 * chord; % height of wing box assumed 10% chord
 width_wingBox = (0.65-0.15) * chord; % width of wing box
 N_cover = (totalMoments ./ (h_wingBox .* width_wingBox))';
 
-% integrally stiffened panels - realistic approach
-close all
-% trial one, fixing values of b, R_t and R_b
-R_b_1 = 0.65; R_t_1 = 2.25; sigma_ratio_1 = 1.6;
+% L = 0.1; % intial guess, needs to be optimised as well
 
-F_new_1 = 1.314 * ((R_b_1^3 * R_t_1 * (4 + R_b_1 * R_t_1))^0.25)/(1 + R_b_1 * R_t_1) * sigma_ratio_1^0.25;
+% % optimisation time
+% for i = 1:5
+%     R_t1 = R_t(i);
+%     for j = 1:6
+%         R_b1 = R_b(j);
+%         F(i,j) = 1.314 * ((R_b1^3 * R_t1 * (4 + R_b1 * R_t1))^0.25) / (1 + R_b1 * R_t1) * sigma_ratio(i,j)^0.25;
+%         % calculating ideal pitch at the root and setting this as constant
+%         %for the rest of the wing. 
+%         b(i,j) = 1.103 * ((sqrt(F(i,j))*(1 + R_b1 * R_t1)/((R_b1^3 * R_t1 * (4 + R_b1 * R_t1))^0.5))) * (N_cover(1)' * L^3 / E_skin)^0.25;
+%         
+%         % skin thickness and stress at root WITHOUT stringers
+%         t1_nada(i,j) = ((N_cover(1) * L^2) / (3.62 * E_skin))^(1/3);
+%         sigma_nada(i,j) = N_cover(1)/t1_nada(i,j);
+%         
+%         % skin thickness and stress at root WITH stringers
+%         t1_with(i,j) = ((N_cover(1) * b(i,j)^2) / (sigma_ratio(i,j) * 3.62 * E_skin * (1 + R_b1 * R_t1))).^(1/3);
+%         % t1_with(i,j) = (1 / (F(i,j) * (1 + R_t1 * R_b1))) * (N_cover(1) * L / E_skin)^(1/2);
+%         sigma_with(i,j) = N_cover(1)/(t1_with(i,j) * (1+R_t1 * R_b1));
+%         h_stringer(i,j) = b(i,j) * R_b1;
+%         t_stringer(i,j) = R_t1 * t1_with(i,j);
+%         A_s(i,j) = t_stringer(i,j) * b(i,j);
+%         t_effective(i,j) = (t1_with(i,j) + A_s(i,j) / b(i,j));
+%         sigma_z(i,j) = N_cover(1) / t_effective(i,j);
+%         
+%         % euler buckling
+% %         y(i,j) = b(i,j) - 
+% %         I = (1/3) * (t_with(i,j) * y(i,j)^3 + b(i,j) * (b(i,j) - y(i,j))^3 - (b(i,j) - y(i,j)) * (b(i,j) - y(i,j) - t_with(i,j))^3)
+% %         A_column(i,j) = t_stringer(i,j) * h_stringer(i,j); % area of column
+% %         I(i,j) = t_stringer(i,j) * h_stringer(i,j)^3 / 12;
+% %         sigma_euler(i,j) = (pi^2 * E_skin * I(i,j)) / (4 * h_stringer(i,j)^2 * A_column(i,j));
+%         rho_squared(i,j) = (b(i,j)^2 * R_t1 * R_b1^3 * (4 + R_b1 * R_t1)) / (12 * (1 + R_t1 * R_b1)^2);
+%         sigma_euler(i,j) = pi^2 * E_skin  * rho_squared(i,j) / L^2;
+%     end
+% end
 
-%rib sizing
-% rib thickness
-t_rib = ((abs(distLoad) .* (h_wingBox).^2)./(3.62 .* chord .* E_rib)).^(1/3);
+% % decide on ratio selection
+% R_b_trial = R_b(3); R_t_trial = R_t(3); sigma_ratio_trial = sigma_ratio(3,3);
+% 
+% F_trial = 1.314 * ((R_b_trial^3 * R_t_trial * (4 + R_b_trial * R_t_trial))^0.25) / (1 + R_b_trial * R_t_trial) * sigma_ratio_trial^0.25;
+% b_trial = 1.103 * ((sqrt(F_trial)*(1 + R_b_trial * R_t_trial)/((R_b_trial^3 * R_t_trial * (4 + R_b_trial * R_t_trial))^0.5))) * (N_cover(1) * L^3 / E_skin)^0.25;
+% t1_with_trial = ((N_cover .* b_trial.^2) ./ (sigma_ratio_trial .* 3.62 * E_skin .* (1 + R_b_trial .* R_t_trial))).^(1/3);
+% t_stringer_trial = R_t_trial * t1_with_trial;
+% h_stringer_trial = R_b_trial * b_trial;
+% sigma_with_trial = N_cover ./ (t1_with_trial * (1 + R_t_trial * R_b_trial));
+% 
+% 
+% figure
+% hold on
+% stairs(x,t1_with_trial)
+% stairs(x,t_stringer_trial)
+% stairs(x,ones(100,1) .* h_stringer_trial)
+% hold off
+% 
+% figure
+% hold on
+% plot(x,sigma_with_trial)
+% plot([x(1),x(100)],[417*10^6, 417*10^6])
+% hold off
+
+L = 0; % intial guess, needs to be optimised as well
 figure
-plot(x,t_rib)
+% optimisation time
+for k = 1:100
+    
+    for i = 1:5
+        R_t1 = R_t(i);
+        
+        for j = 1:6
+            R_b1 = R_b(j);
+            
+            for L = 0.1:0.001:5
+                % Farrar Efficiency
+                F(i,j) = 1.314 * ((R_b1^3 * R_t1 * (4 + R_b1 * R_t1))^0.25) / (1 + R_b1 * R_t1) * sigma_ratio(i,j)^0.25;
+                
+                % calculating ideal pitch at the root and setting this as constant
+                %for the rest of the wing.
+                b(i,j) = 1.103 * ((sqrt(F(i,j))*(1 + R_b1 * R_t1)/((R_b1^3 * R_t1 * (4 + R_b1 * R_t1))^0.5))) * (N_cover(1) * L^3 / E_skin)^0.25;
+                
+                % calculating how Farrar efficiency varies along Span with
+                % pitch set constant to value at root
+                F_variable(i,j,k) = ((b(i,j)^2 * R_b1^3 * R_t1 * (4 + R_b1 * R_t1)) / (1.103^2 * (1 + R_b1 * R_t1)^2)) * (E_skin / (N_cover(k) * L^3))^(1/2);
+                
+                % skin thickness and stress at root WITHOUT stringers
+                t1_nada(i,j,k) = ((N_cover(k) * L^2) / (3.62 * E_skin))^(1/3);
+                
+                % skin thickness and stress at root WITH stringers
+                t1_with(i,j,k) = ((N_cover(k) * b(i,j)^2) / (sigma_ratio(i,j) * 3.62 * E_skin * (1 + R_b1 * R_t1))).^(1/3);
+                plot(x, t1_with(3,3))
+                
+                sigma_with(i,j,k) = N_cover(k)/(t1_with(i,j,k) * (1+R_t1 * R_b1));
+                t_stringer(i,j,k) = R_t1 * t1_with(i,j,k);
+                h_stringer(i,j,k) = R_b1 * b(i,j);
+                
+                t_e(i,j,k) = (t1_with(i,j,k) * (1 + R_t1 * R_b1));
+                
+                I(i,j,k) = 2 .* t_e(i,j,k) .* chord(k) .* h_wingBox(k) ./ 2;
+                F_crush(i,j,k) = (totalMoments(k).^2 .* h_wingBox(k) .* L .* t_e(i,j,k).* chord(k)) ./ (2 .* E_skin * I(i,j,k).^2);
+                t_rib(i,j,k) = ((F_crush(i,j,k) .* h_wingBox(k).^2) ./ (3.62 * E_skin)).^(1/3);
+                
+                L_iteration = (4 .* F_variable(i,j,k)^2 .* h_wingBox(k).^2 .* t_rib(i,j,k).^2 .* E_rib ./ N_cover(k)).^(1/3);
+                
+                eps = L_iteration - L;
+                
+                if eps<0.001
+                    L_final(i,j,k) = L_iteration;
+                    break
+                end
+            end
+            
+        end
+    end
+end
 
-% rib spacing % farrar efficieny not constant!
-L = (4 .* F_new_1^2 .* width_wingBox.^2 .* t_rib.^2 .* E_rib ./ N_cover').^(1/3);
-figure
-plot(x,L)
-L_min = min(L)
+% R_b = 0.6; R_t = 2.0; F = 0.8059; sigma_ratio = 1.47566;
+% a1 = ((4 * F^2 * h_wingBox(1)^2 * h_wingBox(1)^(2/3) * E_skin)/(N_cover(1) * (3.62 * E_skin)^(2/3)))^(2/3);
+% a2 = ((totalMoments(1)^2 * h_wingBox(1) * chord(1))/(8 * width_wingBox(1)^2 * E_skin * (h_wingBox(1)/2)^4)) ^ (2/9);
+% a3 = (1+R_b * R_t)^(-2/9);
+% a4 = ((N_cover(1))/(sigma_ratio * 3.62 * E_skin * (1 + R_b * R_t)))^(-2/27);
+% a5  = (1.103 * ((sqrt(F)*(1 + R_b * R_t)/((R_b^3 * R_t * (4 + R_b * R_t))^0.5))) * (N_cover(1) / E_skin)^0.25)^(-2/27);
+% 
+% L_niklas = (a1*a2*a3*a4*a5)^(1 / (1 - 2/9 + 6/108))
 
-% stringer pitch optimised for the root
-b_new_1 = 1.103 * ((sqrt(F_new_1)*(1 + R_b_1 * R_t_1)/((R_b_1^3 * R_t_1 * (4 + R_b_1 * R_t_1))^0.5))) .* (N_cover(1)' .* L_min^3 ./ E_skin).^0.25;
+% % corresponding rib thickness for trial
+% % crushing force
+% t_e_trial = (t1_with_trial * (1 + R_t_trial * R_b_trial));
+% I_trial = 2 .* t_e_trial' .* chord .* h_wingBox ./ 2;
+% F_crush_trial = (totalMoments.^2 .* h_wingBox .* L .* t_e_trial' .* chord) ./ (2 .* E_skin * I_trial.^2);
+% t_rib = ((F_crush_trial .* h_wingBox.^2) ./ (3.62 * E_skin)).^(1/3);
+% 
+% figure
+% plot(x,t_rib)
+% 
+% % optimal rib spacing
+% L_trial = (4 .* F_trial^2 .* width_wingBox.^2 .* t_rib.^2 .* E_rib ./ N_cover').^(1/3);
+% figure
+% plot(x,L_trial)
 
-% WITHOUT STRINGERS
-t1_nada = ((N_cover.*L_min^2) ./ (3.62 * E_skin)).^(1/3);
-A_s_nada = L_min .* t1_nada;
-sigma_nada = N_cover./t1_nada;
+% figure
+% hold on
+% plot(sigma_with,'rx')
+% plot(sigma_nada,'bo')
 
-% WITH STRINGERS
-t_new_1 = ((N_cover .* b_new_1^2) ./ (sigma_ratio_1 * 3.62 * E_skin * (1 + R_b_1 * R_t_1))).^(1/3);
-A_s = t_new_1 * b_new_1;
-T_effective = (t_new_1 + A_s./b_new_1);
-sigma_new_1 = N_cover./(t_new_1 * (1+R_t_1 * R_b_1));
-sigma_z = N_cover./T_effective;
-
-% yield skin thickness
-t_failure  = N_cover ./ (520*10^6);
-
-% plotting thickness distriutions
-figure
-hold on
-stairs(x,t1_nada,'r');
-stairs(x,t_new_1,'b');
-stairs(x,t_failure,'g');
-hold off
-
-figure
-hold on
-plot(x,sigma_nada);
-plot(x,sigma_new_1);
-plot(x,sigma_z);
-plot([x(1), x(100)], [434.5*10^6 434.5*10^6])
-legend('no stringers','stringers included','sigma_z','sigma yield')
-title('stress variation along half span')
-hold off
+%%
+% % integrally stiffened panels - realistic approach
+% close all
+% % trial one, fixing values of b, R_t and R_b
+% R_b_1 = 0.65; R_t_1 = 2.25; sigma_ratio_1 = 1.6;
+% 
+% F_new_1 = 1.314 * ((R_b_1^3 * R_t_1 * (4 + R_b_1 * R_t_1))^0.25)/(1 + R_b_1 * R_t_1) * sigma_ratio_1^0.25;
+% 
+% L = 0.5;
+% 
+% % stringer pitch optimised for the root
+% b_new_1 = 1.103 * ((sqrt(F_new_1)*(1 + R_b_1 * R_t_1)/((R_b_1^3 * R_t_1 * (4 + R_b_1 * R_t_1))^0.5))) .* (N_cover(1)' .* L^3 ./ E_skin).^0.25;
+% 
+% % WITHOUT STRINGERS
+% t1_nada = ((N_cover.*L^2) ./ (3.62 * E_skin)).^(1/3);
+% A_s_nada = L .* t1_nada;
+% sigma_nada = N_cover./t1_nada;
+% 
+% % WITH STRINGERS
+% t_new_1 = ((N_cover .* b_new_1^2) ./ (sigma_ratio_1 * 3.62 * E_skin * (1 + R_b_1 * R_t_1))).^(1/3);
+% A_s = t_new_1 * b_new_1;
+% T_effective = (t_new_1 + A_s./b_new_1);
+% sigma_new_1 = N_cover./(t_new_1 * (1+R_t_1 * R_b_1));
+% sigma_z = N_cover./T_effective;
+% 
+% % yield skin thickness
+% t_failure  = N_cover ./ (520*10^6);
+% 
+% % plotting thickness distriutions
+% figure
+% hold on
+% stairs(x,t1_nada,'r');
+% stairs(x,t_new_1,'b');
+% stairs(x,t_failure,'g');
+% hold off
+% 
+% figure
+% hold on
+% plot(x,sigma_nada);
+% plot(x,sigma_new_1);
+% plot(x,sigma_z);
+% plot([x(1), x(100)], [434.5*10^6 434.5*10^6])
+% legend('no stringers','stringers included','sigma_z','sigma yield')
+% title('stress variation along half span')
+% hold off
 % SANITY CHECK
 % t_new_1 = ((900000 .* b_new_1^2) ./ (1.75 * 3.62 * (70 * 10^9) * (1 + R_b_1 * R_t_1))).^(1/3);
 
-% buckling stress
-sigma_crit = N_cover ./ (t_new_1 * (1+R_t_1 * R_b_1));
+% % buckling stress
+% sigma_crit = N_cover ./ (t_new_1 * (1+R_t_1 * R_b_1));
 
 % SANITY CHECK
 % sigma_crit = 900000 ./ (t_new_1 * (1+R_t_1 * R_b_1));
@@ -473,25 +666,41 @@ sigma_crit = N_cover ./ (t_new_1 * (1+R_t_1 * R_b_1));
 % t_failure  = N_cover./(520*10^6);
 % plot(x,t_failure,'g')
 % hold off
-%% rib sizing
-% close all
-
-h_wingBox = 0.10 * chord; % height of wing box assumed 10% chord
-width_wingBox = (0.65-0.15) * chord; % width of wing box
-
-% rib thickness
-t_rib = ((abs(distLoad) .* (h_wingBox).^2)./(3.62 .* chord .* E_rib)).^(1/3);
-figure
-plot(x,t_rib)
-
-% rib spacing
-L = (4 .* 0.81^2 .* width_wingBox.^2 .* t_rib.^2 .* E_rib ./ N_cover').^(1/3);
-figure
-plot(x,L)
-L_min = min(L)
-
-
-% stress in ribs
-sigma_ribs = distLoad ./ (t_rib .* chord);
-
-% crushing force
+% %% rib sizing
+% % close all
+% 
+% h_wingBox = 0.10 * chord; % height of wing box assumed 10% chord
+% width_wingBox = (0.65-0.15) * chord; % width of wing box
+% 
+% % rib thickness
+% t_rib = ((abs(distLoad) .* (h_wingBox).^2)./(3.62 .* chord .* E_rib)).^(1/3);
+% figure
+% plot(x,t_rib)
+% 
+% % rib spacing
+% L = (4 .* 0.81^2 .* width_wingBox.^2 .* t_rib.^2 .* E_rib ./ N_cover').^(1/3);
+% figure
+% plot(x,L)
+% L_min = min(L)
+% 
+% 
+% % stress in ribs
+% sigma_ribs = distLoad ./ (t_rib .* chord);
+% 
+% % crushing force
+% 
+% % %%
+% % %rib sizing
+% % % determining crushing force F_crush
+% % L_trial = 0.5
+% % 
+% % % rib thickness
+% % t_rib = ((abs(distLoad) .* (h_wingBox).^2)./(3.62 .* chord .* E_rib)).^(1/3);
+% % figure
+% % plot(x,t_rib)
+% % 
+% % % rib spacing % farrar efficieny not constant!
+% % L = (4 .* F_new_1^2 .* width_wingBox.^2 .* t_rib.^2 .* E_rib ./ N_cover').^(1/3);
+% % figure
+% % plot(x,L)
+% % L_min = min(L)
